ee56574ed2fb138db17b47386f988580338b9190
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-07-07 15:48:32 +0900

    fix DlgInitRes::LoadFromStream signedness warning

--- src/DlgInitRes.cpp
+++ src/DlgInitRes.cpp
@@ -23,7 +23,7 @@ bool DlgInitRes::LoadFromStream(const MByteStreamEx& stream)
 		if (!stream.ReadWord(entry.wMsg) || !stream.ReadDword(dwLen))
 			return false;
 
-		if (dwLen > LONG_MAX / sizeof(WCHAR))
+		if (dwLen > DWORD(LONG_MAX / sizeof(WCHAR)))
 			return false;
 
 		assert(dwLen);
