8b13ac41aa71c95f23a67d654598c5e1fc46a27f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-07-22 11:36:13 +0900

    improve popup menu

--- src/MMainWnd_Command.cpp
+++ src/MMainWnd_Command.cpp
@@ -1022,11 +1022,24 @@ void MMainWnd::OnContextMenu(HWND hwnd, HWND hwndContext, UINT xPos, UINT yPos)
 	TreeView_SelectItem(hwndContext, hItem);
 
 	// load the menu
-	HMENU hMenu = LoadMenuW(m_hInst, MAKEINTRESOURCEW(IDR_POPUPMENUS));
+	HMENU hMenu = LoadMenuW(m_hInst, MAKEINTRESOURCEW(IDR_MAINMENU));
 	OnInitMenu(hwnd, hMenu);
-	HMENU hSubMenu = ::GetSubMenu(hMenu, 0);
+	HMENU hSubMenu = ::GetSubMenu(hMenu, 1);
 	if (hMenu == NULL || hSubMenu == NULL)
 		return;
+
+	UINT cItems = ::GetMenuItemCount(hSubMenu);
+	BOOL iFound = -1;
+	for (UINT iItem = 0; iItem < cItems; ++iItem)
+	{
+		if (::GetMenuItemID(hSubMenu, iItem) == ID_DELETERES)
+		{
+			iFound = iItem;
+			break;
+		}
+	}
+	while (::DeleteMenu(hSubMenu, iFound + 1, MF_BYPOSITION))
+		;
 
 	EntryBase *entry = g_res.get_entry();
 	if (!entry || !GetTreeItemHelp(entry))
